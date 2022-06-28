9ebbe79ee4a7aaacdb7e32b3d0e7eab9979a5cc4
AkhileshAdithya <obliviongod16@gmail.com>
2022-06-29 02:29:25 +0530

    added console thingy

---
+++ madeByAkhilesh.js
@@ -0,0 +1,6 @@
+console.clear()
+console.log("%c Made with ❤️ by Akhilesh", 'font-size: 32px; font-weight: bold')
+console.log("%c ꧁꧁꧁𖤍𖤓  Follow me on Github  𖤓𖤍꧂꧂꧂", 'font-size: 14px;')
+
+console.log("Github : https://github.com/AkhileshAdithya/")
+            
